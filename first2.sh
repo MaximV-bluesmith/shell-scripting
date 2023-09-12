89a6e70253a3c0091bd4104c443a0ce5b88e45b6
Maxim v <maxim.vakourov@bluesmithis.com>
2023-09-12 14:32:10 +0100

    Script for reasoning on quoting

---
+++ first2.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Here is a bunch of Hello Worlds
+echo "Hello 	World"
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello 	World
+echo "Hello" World
+echo Hello "	" World
+echo "Hello" * "World"
+echo `hello` world
+echo 'hellow' world
