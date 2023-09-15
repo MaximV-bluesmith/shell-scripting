12388568133bd9ff2524ccbd6b2969f6641d0c86
Maxim v <maxim.vakourov@bluesmithis.com>
2023-09-15 09:12:52 +0100

    Changed ex7.bash to also complete ex8

--- ex7.bash
+++ ex7.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+shopt -s extglob
 mkdir -p old 
-cp {0..11}.txt old/
-rename 's/\.txt$/\.old/' old/*
+cp +([0-9])*.txt old/
+rename 's/\.txt$/\.old/' old/* # Not part of coreutils, may need installing
